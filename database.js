0401e17dfc39a86a1197cad8213d5e392c450abf
Joyce Elauria <joyce.elauria@auth0.com>
2021-06-11 10:46:37 -0700

    merge backend into main

--- database.js
+++ database.js
@@ -1,82 +1,46 @@
-<<<<<<< HEAD
-const data = [
-    {
-        id: 0,
-        sound_file: 'bird_chirp.wav',
-        image_file: 'https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png',
-=======
 export const data = [
     {
         id: 0,
         sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/qV5mnHuLnN.wav',
         image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/lLtucPhsEk.png',
->>>>>>> 8ebf1dfaa062a025e3f8fa22532f5a72032d84d9
         question: 'What type of animal is this?',
         answer: 'bird'
     },
     {
         id: 1,
-<<<<<<< HEAD
-        sound_file: 'car_honk.wav',
-        image_file: 'car.png',
-=======
         sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/AvdBHZGIrK.wav',
         image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/ajqNicI3TQ.png',
->>>>>>> 8ebf1dfaa062a025e3f8fa22532f5a72032d84d9
         question: 'What type of object is this?',
         answer: 'car'
     },
     {
         id: 2,
-<<<<<<< HEAD
-        sound_file: 'cat_meow.wav',
-        image_file: 'cat.png',
-=======
         sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/l5YEOzHklO.wav',
         image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/ap7gSm6g3e.png',
->>>>>>> 8ebf1dfaa062a025e3f8fa22532f5a72032d84d9
         question: 'What type of animal is this?',
         answer: 'cat'
     },
     {
         id: 3,
-<<<<<<< HEAD
-        sound_file: 'dog_bark.wav',
-        image_file: 'dog.png',
-=======
         sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/kBjUpCWB1N.wav',
         image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/6BWza1iGYu.png',
->>>>>>> 8ebf1dfaa062a025e3f8fa22532f5a72032d84d9
         question: 'What type of animal is this?',
         answer: 'dog'
     },
     {
         id: 4,
-<<<<<<< HEAD
-        sound_file: 'sodacan_open.wav',
-        image_file: 'sodacan.png',
-=======
         sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/ko3qFDnOAl.wav',
         image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/YIVt5fjqFg.png',
->>>>>>> 8ebf1dfaa062a025e3f8fa22532f5a72032d84d9
         question: 'What type of object is this?',
         answer: 'soda can'
     },
     {
         id: 5,
-<<<<<<< HEAD
-        sound_file: 'trumpet_horn.wav',
-        image_file: 'trumpet.png',
-=======
         sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/maXVusCfXa.wav',
         image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/C66y5EM5gE.png',
->>>>>>> 8ebf1dfaa062a025e3f8fa22532f5a72032d84d9
         question: 'What type of object is this?',
         answer: 'trumpet'
     }
 ];
-<<<<<<< HEAD
 
 module.exports = data;
-=======
->>>>>>> 8ebf1dfaa062a025e3f8fa22532f5a72032d84d9
