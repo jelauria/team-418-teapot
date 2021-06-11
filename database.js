8ebf1dfaa062a025e3f8fa22532f5a72032d84d9
croseux <85645926+croseux@users.noreply.github.com>
2021-06-11 13:13:23 -0400

    Update database.js

--- database.js
+++ database.js
@@ -8,36 +8,36 @@ export const data = [
     },
     {
         id: 1,
-        sound_file: 'car_honk.wav',
-        image_file: 'car.png',
+        sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/AvdBHZGIrK.wav',
+        image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/ajqNicI3TQ.png',
         question: 'What type of object is this?',
         answer: 'car'
     },
     {
         id: 2,
-        sound_file: 'cat_meow.wav',
-        image_file: 'cat.png',
+        sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/l5YEOzHklO.wav',
+        image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/ap7gSm6g3e.png',
         question: 'What type of animal is this?',
         answer: 'cat'
     },
     {
         id: 3,
-        sound_file: 'dog_bark.wav',
-        image_file: 'dog.png',
+        sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/kBjUpCWB1N.wav',
+        image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/6BWza1iGYu.png',
         question: 'What type of animal is this?',
         answer: 'dog'
     },
     {
         id: 4,
-        sound_file: 'sodacan_open.wav',
-        image_file: 'sodacan.png',
+        sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/ko3qFDnOAl.wav',
+        image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/YIVt5fjqFg.png',
         question: 'What type of object is this?',
         answer: 'soda can'
     },
     {
         id: 5,
-        sound_file: 'trumpet_horn.wav',
-        image_file: 'trumpet.png',
+        sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/maXVusCfXa.wav',
+        image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/C66y5EM5gE.png',
         question: 'What type of object is this?',
         answer: 'trumpet'
     }
