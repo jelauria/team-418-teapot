691a33783bc4772089e45d1c7f4aaa99b63b655e
Joyce Elauria <joyce.elauria@auth0.com>
2021-06-11 10:01:03 -0700

    added database

---
+++ database.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        id: 0,
+        sound_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/sound_files/qV5mnHuLnN.wav',
+        image_file: 'https://raw.githubusercontent.com/jelauria/team-418-teapot/main/image_files/lLtucPhsEk.png',
+        question: 'What type of animal is this?',
+        answer: 'bird'
+    },
+    {
+        id: 1,
+        sound_file: 'car_honk.wav',
+        image_file: 'car.png',
+        question: 'What type of object is this?',
+        answer: 'car'
+    },
+    {
+        id: 2,
+        sound_file: 'cat_meow.wav',
+        image_file: 'cat.png',
+        question: 'What type of animal is this?',
+        answer: 'cat'
+    },
+    {
+        id: 3,
+        sound_file: 'dog_bark.wav',
+        image_file: 'dog.png',
+        question: 'What type of animal is this?',
+        answer: 'dog'
+    },
+    {
+        id: 4,
+        sound_file: 'sodacan_open.wav',
+        image_file: 'sodacan.png',
+        question: 'What type of object is this?',
+        answer: 'soda can'
+    },
+    {
+        id: 5,
+        sound_file: 'trumpet_horn.wav',
+        image_file: 'trumpet.png',
+        question: 'What type of object is this?',
+        answer: 'trumpet'
+    }
+];
